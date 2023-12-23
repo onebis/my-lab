6348919f4616e099a4962ee17946b07b47e01775
minoru horiuchi <noru238@gmail.com>
2023-12-23 11:37:51 +0900

    headerホバー時実装

--- app/components/example1/Header.tsx
+++ app/components/example1/Header.tsx
@@ -1,38 +1,57 @@
 import { Button } from '@nextui-org/button'
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@nextui-org/navbar'
+import classes from '@/app/components/example1/example1.module.css'
 import { Account } from '@/public/icon/account/Account'
 import { MagnifyingGlass } from '@/public/icon/magnifying-glass/magnifying-glass'
 import { ShoppingCart } from '@/public/icon/shopping_cart/shopping_cart'
 
+const header_lists = [
+  {
+    title: 'home',
+    url: '#',
+  },
+  {
+    title: 'about',
+    url: '#',
+  },
+  {
+    title: 'products',
+    url: '#',
+  },
+  {
+    title: 'shop',
+    url: '#',
+  },
+  {
+    title: 'service',
+    url: '#',
+  },
+  {
+    title: 'faq',
+    url: '#',
+  },
+  {
+    title: 'contact',
+    url: '#',
+  },
+]
+
 export function Header() {
   return (
-    // <header className=' flex h-1/6 w-full px-5'>
     <Navbar position={'sticky'} className='flex w-full justify-center'>
       <NavbarContent>
         <div className='w-auto text-center'>Logo</div>
       </NavbarContent>
       <NavbarContent className='space-x-5 uppercase'>
-        <NavbarItem className='sm:text-sm lg:text-base'>
-          <a href={'#'}>home</a>
-        </NavbarItem>
-        <NavbarItem className='sm:text-sm lg:text-base'>
-          <a href={'#'}>about</a>
-        </NavbarItem>
-        <NavbarItem className='sm:text-sm lg:text-base'>
-          <a href={'#'}>products</a>
-        </NavbarItem>
-        <NavbarItem className='sm:text-sm lg:text-base'>
-          <a href={'#'}>shop</a>
-        </NavbarItem>
-        <NavbarItem className='sm:text-sm lg:text-base'>
-          <a href={'#'}>service</a>
-        </NavbarItem>
-        <NavbarItem className='sm:text-sm lg:text-base'>
-          <a href={'#'}>faq</a>
-        </NavbarItem>
-        <NavbarItem className='sm:text-sm lg:text-base'>
-          <a href={'#'}>contact</a>
-        </NavbarItem>
+        {header_lists.map((item) => {
+          return (
+            <NavbarItem key={item.title} className='sm:text-sm lg:text-base'>
+              <a className={classes.header_item + ' pb-1'} href={item.url}>
+                {item.title}
+              </a>
+            </NavbarItem>
+          )
+        })}
       </NavbarContent>
       <NavbarContent justify={'end'} className='gap-0.5'>
         <NavbarItem>
@@ -52,6 +71,5 @@ export function Header() {
         </NavbarItem>
       </NavbarContent>
     </Navbar>
-    // </header>
   )
 }
