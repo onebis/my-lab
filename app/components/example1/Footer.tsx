88810116191e9fa18aeb38a03512ed6a7ddd1c3d
minoru horiuchi <noru238@gmail.com>
2024-01-14 10:55:23 +0900

    footer responsive

--- app/components/example1/Footer.tsx
+++ app/components/example1/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className='mt-10 w-full bg-gray-400'>
       <div className='mx-auto mt-20 max-w-5xl px-20'>
-        <div className='flex justify-between gap-2'>
+        <div className='hidden md:flex md:justify-between md:gap-2'>
           <div className='flex flex-col'>
             <div className='w-52 border-b-1 border-b-amber-200 pb-1 pt-1'>
               <a href='#' className=' font-playfair text-xl  uppercase hover:opacity-60'>
@@ -45,6 +45,48 @@ export default function Footer() {
             </div>
           </div>
         </div>
+
+        {/* responsive */}
+        <div className='flex justify-center gap-1 md:hidden'>
+          <div className='flex flex-col'>
+            <div className='w-52 border-b-1 border-b-amber-200 pb-1'>
+              <a href='#' className=' font-playfair uppercase hover:opacity-60'>
+                home
+              </a>
+            </div>
+            <div className='mt-2 w-52 border-b-1 border-b-amber-200 pb-1'>
+              <a href='#' className=' font-playfair uppercase hover:opacity-60'>
+                about
+              </a>
+            </div>
+            <div className='mt-2 w-52 border-b-1 border-b-amber-200 pb-1'>
+              <a href='#' className=' font-playfair uppercase hover:opacity-60'>
+                shop
+              </a>
+            </div>
+            <div className='mt-2 w-52 border-b-1 border-b-amber-200 pb-1'>
+              <a href='#' className=' font-playfair uppercase hover:opacity-60'>
+                series
+              </a>
+            </div>
+            <div className='mt-2 w-52 border-b-1 border-b-amber-200 pb-1'>
+              <a href='#' className=' font-playfair uppercase hover:opacity-60'>
+                service
+              </a>
+            </div>
+            <div className='mt-2 w-52 border-b-1 border-b-amber-200 pb-1'>
+              <a href='#' className=' font-playfair uppercase hover:opacity-60'>
+                faq
+              </a>
+            </div>
+            <div className='mt-2 w-52 border-b-1 border-b-amber-200 pb-1'>
+              <a href='#' className=' font-playfair uppercase hover:opacity-60'>
+                contact
+              </a>
+            </div>
+          </div>
+        </div>
+
         <div className='mt-10 text-center text-gray-200'>
           <p>Your company description here.</p>
         </div>
